03f50ec746802e1ccd40430068104526f8c3c68c
Kevinkien25 <kevinkien2500@gmail.com>
2025-12-06 11:35:32 +0700

    thêm đc cái chức năng của GVBM và GVCN, còn thời khóa biểu thì ko hiểu lắm gặp lỗi

--- routes/phuHuynhRoute.js
+++ routes/phuHuynhRoute.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const paymentController = require('../controllers/phuhuynh/paymentController');
 const gradesCtrl = require('../controllers/phuhuynh/gradesController');
 const tuyenSinhController = require('../controllers/phuhuynh/tuyenSinhController');
+const scheduleController = require('../controllers/scheduleController');
+
 // Route hiển thị trang tổng quan công nợ/thanh toán
 router.get('/thanh-toan-hoc-phi', paymentController.showPaymentDashboard);
 router.post('/thanh-toan-hoc-phi/initiate', paymentController.initiatePayment);
@@ -14,11 +16,12 @@ router.post('/thanh-toan-hoc-phi/initiate', paymentController.initiatePayment);
 // Xem điểm chi tiết theo học sinh (có thể dùng cho học sinh riêng)
 router.get('/diem/:hocSinhId', gradesCtrl.showGradesForStudent);
 
-
-
 // Hiển thị form đăng ký tuyển sinh
 router.get('/dangky', tuyenSinhController.showFormDangKy);
 
 // Xử lý submit form đăng ký
 router.post('/dangky', tuyenSinhController.submitDangKy);
+
+// Thêm vào routes hiện có
+router.get('/thoikhoabieu', scheduleController.viewScheduleParent);
 module.exports = router;
